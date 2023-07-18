484f75350d99dccd7e421b2e669564ee2fa6c0a3
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-19 00:16:27 +0330

    add collection view in explore view

---
+++ Foodino/Explore/ExploreViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ExploreViewController.swift
+//  Foodino
+//
+//  Created by Abolfazl-Atena on 4/27/1402 AP.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ExploreViewController : UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        return header
+    }
+    
+    
+}
