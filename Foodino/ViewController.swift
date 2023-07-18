6f6c185fe3c3e81ad5ec7361b4a268092b53420e
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-18 22:22:36 +0330

    Initial Commit

---
+++ Foodino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodino
+//
+//  Created by Abolfazl-Atena on 4/27/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodinoUITests/FoodinoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodinoUITestsLaunchTests.swift
+//  FoodinoUITests
+//
+//  Created by Abolfazl-Atena on 4/27/1402 AP.
+//
+
+import XCTest
+
+final class FoodinoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
