a21d1ef61e5ce31f95c9dc5f40503e70f563d0ad
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-26 01:07:27 +0330

    add location view controller

---
+++ Foodino/Location/LocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationViewController.swift
+//  Foodino
+//
+//  Created by Abolfazl-Atena on 5/4/1402 AP.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
