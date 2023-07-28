e5e6ea03a36d9395c432bd56e7cbec01025a8332
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-28 21:46:26 +0330

    add load location data method

---
+++ Foodino/Location/Model/LocationDataManager.swift
@@ -0,0 +1,26 @@
+//
+//  LocationDataManager.swift
+//  Foodino
+//
+//  Created by Abolfazl-Atena on 5/6/1402 AP.
+//
+
+import Foundation
+
+
+class LocationDataManager {
+    private var locations : [String] = []
+    
+    func loadData() -> [[String:String]] {
+        // This is used to decode plist file's data
+   
+        guard let path = Bundle.main.path(forResource: "Locations", ofType: ".plist"),
+  
+        let locationData = FileManager.default.contents(atPath: path),
+        
+        let locations = try? PropertyListDecoder().decode([[String:String]].self, from: locationData) else {
+            return []
+        }
+        return locations
+    }
+}
