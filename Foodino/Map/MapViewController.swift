b57825b4db507652b78d8ac59957af8c28810f08
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-30 00:36:47 +0330

    add custom map annotation

--- Foodino/Map/MapViewController.swift
+++ Foodino/Map/MapViewController.swift
@@ -16,11 +16,10 @@ class MapViewController: UIViewController,MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         initialize()
-      
-
     }
     
     func initialize() {
+        mapView.delegate = self
         manager.fetch(completion:{(anotations) in
             setupMap(anotations)
         })
@@ -29,5 +28,36 @@ class MapViewController: UIViewController,MKMapViewDelegate {
     func setupMap(_ anotations: [RestaurantItem])  {
         mapView.setRegion(manager.initialRegion(latDelta: 0.5, longDelta: 0.5), animated: true)
         mapView.addAnnotations(manager.anotations)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation:
+    MKAnnotation) -> MKAnnotationView? {
+       let identifier = "custompin"
+       guard !annotation.isKind(of: MKUserLocation.self)
+       else {
+          return nil
+       }
+       let annotationView: MKAnnotationView
+       if let customAnnotationView =
+       mapView.dequeueReusableAnnotationView(
+       withIdentifier: identifier) {
+          annotationView = customAnnotationView
+          annotationView.annotation = annotation
+       } else {
+          let av = MKAnnotationView(annotation:
+          annotation, reuseIdentifier: identifier)
+          av.rightCalloutAccessoryView =
+          UIButton(type: .detailDisclosure)
+          annotationView = av
+       }
+       annotationView.canShowCallout = true
+       if let image = UIImage(named:
+            "custom-annotation") {
+                annotationView.image = image
+                annotationView.centerOffset = CGPoint(
+                x: -image.size.width / 2,
+                y: -image.size.height / 2)
+       }
+       return annotationView
     }
 }
