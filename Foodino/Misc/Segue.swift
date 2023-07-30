ebc5a647191a2315c83af959c3662a7f8f3d0f59
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-07-30 21:23:27 +0330

    add segues

---
+++ Foodino/Misc/Segue.swift
@@ -0,0 +1,19 @@
+//
+//  Segue.swift
+//  Foodino
+//
+//  Created by Abolfazl-Atena on 5/8/1402 AP.
+//
+
+import Foundation
+
+enum Segue : String {
+    case showDetail
+     case showRating
+     case showReview
+     case showAllReviews
+     case restaurantList
+     case locationList
+     case showPhotoReview
+     case showPhotoFilter
+}
