6938b43531bc5e5a1f394145c6a4840425b44f29
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2023-08-08 00:44:46 +0330

    apply decodable protocol to restaurant item model

--- Foodino/Map/Model/RestaurantItem.swift
+++ Foodino/Map/Model/RestaurantItem.swift
@@ -8,7 +8,7 @@
 import UIKit
 import MapKit
 
-class RestaurantItem: NSObject, MKAnnotation {
+class RestaurantItem: NSObject, MKAnnotation,Decodable {
    
     let name: String?
     let cuisines: [String]
@@ -40,15 +40,16 @@ class RestaurantItem: NSObject, MKAnnotation {
             return cuisines.joined(separator:  ", ")
         }
     }
-    init(dict: [String:AnyObject]) {
-        self.lat = dict["lat"] as? Double
-          self.long = dict["long"] as? Double
-          self.name = dict["name"] as? String
-          self.cuisines = dict["cuisines"] as? [String] ?? []
-          self.address = dict["address"] as? String
-          self.postalCode = dict["postalCode"] as? String
-          self.state = dict["state"] as? String
-          self.imageURL = dict["image_url"] as? String
-          self.restaurantID = dict["id"] as? Int
+    
+    enum CodingKeys: String , CodingKey {
+        case name
+        case cuisines
+        case lat
+        case long
+        case address
+        case postalCode = "postal_code"
+        case state
+        case imageURL = "image_url"
+        case restaurantID = "id"
     }
 }
